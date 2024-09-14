f9f0e4f929bc68048dabadc173444d157d5e097b
CatHat <liase810@gmail.com>
2024-09-14 21:45:58 +0200

    added a navbar and the start for the buttons

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 const loadingScreen = document.getElementById('loading-screen')!
+const navbar = document.querySelector('#navbar') as HTMLElement
+const navbarContainer = document.querySelector(
+    '#navbar-container'
+) as HTMLDivElement
+const parallax = document.querySelector('#parallax')! as HTMLElement
 
 window.addEventListener('load', () => {
     loadingScreen.classList.add('fadeOut')
+})
+
+navbarContainer.addEventListener('wheel', (event) => {
+    parallax.scrollTop += event.deltaY * 10
 })
