a2cd9a896a4d0168215f917cd1d97ec031d41f07
CatHat <liase810@gmail.com>
2024-09-17 07:09:32 +0200

    minor adjustment

--- main.ts
+++ main.ts
@@ -10,5 +10,5 @@ window.addEventListener('load', () => {
 })
 
 navbarContainer.addEventListener('wheel', (event) => {
-    parallax.scrollTop += event.deltaY * 10
+    parallax.scrollTop += event.deltaY * 3
 })
