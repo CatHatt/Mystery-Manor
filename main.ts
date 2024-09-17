a91210247f5b66757782e85ce9bb5e97e0ddce9c
CatHat <liase810@gmail.com>
2024-09-17 11:56:54 +0200

    hide parallax sections that are out of screen

--- main.ts
+++ main.ts
@@ -3,12 +3,30 @@ const navbar = document.querySelector('#navbar') as HTMLElement
 const navbarContainer = document.querySelector(
     '#navbar-container'
 ) as HTMLDivElement
-const parallax = document.querySelector('#parallax')! as HTMLElement
+const main = document.querySelector('#parallax')! as HTMLElement
 
 window.addEventListener('load', () => {
     loadingScreen.classList.add('fadeOut')
 })
 
 navbarContainer.addEventListener('wheel', (event) => {
-    parallax.scrollTop += event.deltaY * 3
+    main.scrollTop += event.deltaY * 3
+})
+
+main.addEventListener('scroll', () => {
+    const parallaxSections = main.querySelectorAll('.parallax-section')
+    const mainPos = main.getBoundingClientRect()
+    const mainScrollPos = main.scrollTop
+
+    for (const parallaxSection of parallaxSections) {
+        const sectionPos = parallaxSection.getBoundingClientRect()
+
+        // this finds the relative position of the current element in the loop compared to the main element
+        //                       ↓      ↓      ↓
+        if (mainScrollPos > sectionPos.y + sectionPos.height * 2 - mainPos.y) {
+            parallaxSection.classList.add('hide')
+        } else {
+            parallaxSection.classList.remove('hide')
+        }
+    }
 })
